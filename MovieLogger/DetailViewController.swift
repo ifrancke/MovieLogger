ec720db6917ba8754d3c98e44c3d1aebf3378db5
Isabelle Francke <ifrancke@uvm.edu>
2021-11-01 22:16:23 -0400

    need to add rating to this area

---
+++ MovieLogger/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  MovieLogger
+//
+//  Created by Mac Admin on 11/1/21.
+//  Copyright © 2021 Mac Admin. All rights reserved.
+//
+
+import UIKit
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var reviewField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        return formatter
+    }()
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        titleField.text = item.title
+        reviewField.text = item.review
+        dateLabel.text = "\(item.dateCreated)"
+    }
+}
